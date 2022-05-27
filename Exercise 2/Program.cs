bcd3cfbd77599d6dfbdda35b6742f170862e7f48
Upper <goodp@yandex.ru>
2022-05-28 01:28:57 +0300

    ex2

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9].
+// Найти сумму положительных/отрицательных элементов массива
+int[] array= new int[12];
+int resultPlus=0;
+int resultMinus=0;
+for (int i=1; i<=12; i++)
+{
+    array[i-1]=new Random().Next(-10,10);
+    if (array[i-1]>0)
+    {
+     resultPlus=resultPlus+array[i-1];
+    }
+    else if(array[i-1]<0)
+    {
+     resultMinus=resultMinus+array[i-1];
+    } 
+}
+Console.WriteLine($"Сумма положительных = {resultPlus}");
+Console.WriteLine($"Сумма отрицательных = {resultMinus}");
+Console.WriteLine();
+printArray(array);
+void printArray(int[] array)
+{
+    for (int i=0; i<array.Length;i++)
+  {
+    Console.Write(array[i] + " ");
+  } 
+}  
